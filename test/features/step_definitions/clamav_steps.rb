35e6fa187638d6b86dc9cf62487b6afb34c40e85
Jonathan Hartman <j@p4nt5.com>
2013-01-14 14:36:58 +0000

    Working on the Cucumber tests

--- test/features/step_definitions/clamav_steps.rb
+++ test/features/step_definitions/clamav_steps.rb
@@ -19,7 +19,7 @@ When /^I manually scan a (\w+) file$/ do |file_type|
       "FILE!$H+H*")
   end
   f.close
-  res = %x{clamscan #{f.path}}
+  @res = %x{clamscan #{f.path}}
   f.unlink
 end
 
@@ -34,18 +34,18 @@ When /^I scan a (\w+) file via clamd$/ do |file_type|
       "FILE!$H+H*")
   end
   f.close
-  res = %x{clamdscan #{f.path}}
+  @res = %x{clamdscan #{f.path}}
   f.unlink
 end
 
 Then "ClamAV should detect nothing" do
-  res.should contain "#{f.path}: OK"
-  res.should contain "\nScanned Files: 1\nInfected Files: 0\n"
+  @res.should contain "#{f.path}: OK"
+  @res.should contain "\nScanned Files: 1\nInfected Files: 0\n"
 end
 
 Then "ClamAV should detect a virus" do
-  res.should contain "#{f.path}: Eicar-Test-Signature FOUND\n"
-  res.should contain "\nScanned Files: 1\nInfected Files: 1\n"
+  @res.should contain "#{f.path}: Eicar-Test-Signature FOUND\n"
+  @res.should contain "\nScanned Files: 1\nInfected Files: 1\n"
 end
 
 # vim: ai et ts=2 sts=2 sw=2 ft=ruby fdm=marker
