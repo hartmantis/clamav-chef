b3216fcaa3dd4081be5faa1578387c5e06c99d7b
Jonathan Hartman <j@p4nt5.com>
2013-01-14 16:53:26 +0000

    Fix case and tempfile reads

--- test/features/step_definitions/clamav_steps.rb
+++ test/features/step_definitions/clamav_steps.rb
@@ -17,6 +17,7 @@ When /^I manually scan a (\w+) file$/ do |file_type|
     @f.write("X5O!P%@AP[4\PZX54(P^)7CC)7}$EICAR-STANDARD-ANTIVIRUS-TEST-" +
       "FILE!$H+H*")
   end
+  @f.rewind
   @f.close
   File.chmod(0777, @f)
   @res = %x{clamscan #{@f.path}}
@@ -32,6 +33,7 @@ When /^I scan a (\w+) file via clamd$/ do |file_type|
     @f.write("X5O!P%@AP[4\PZX54(P^)7CC)7}$EICAR-STANDARD-ANTIVIRUS-TEST-" +
       "FILE!$H+H*")
   end
+  @f.rewind
   @f.close
   File.chmod(0777, @f)
   @res = %x{clamdscan #{@f.path}}
@@ -40,12 +42,12 @@ end
 
 Then "ClamAV should detect nothing" do
   @res.must_include "#{@f.path}: OK"
-  @res.must_include "\nInfected Files: 0\n"
+  @res.must_include "\nInfected files: 0\n"
 end
 
 Then "ClamAV should detect a virus" do
   @res.must_include "#{@f.path}: Eicar-Test-Signature FOUND\n"
-  @res.must_include "\nInfected Files: 1\n"
+  @res.must_include "\nInfected files: 1\n"
 end
 
 # vim: ai et ts=2 sts=2 sw=2 ft=ruby fdm=marker
