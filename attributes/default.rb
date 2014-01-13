fa1d182a1985221741ba35647f9ada703ce79cf6
Jonathan Hartman <j@p4nt5.com>
2014-01-13 03:19:46 +0000

    Update style everywhere else

--- attributes/default.rb
+++ attributes/default.rb
@@ -2,7 +2,7 @@
 # Cookbook Name:: clamav
 # Attributes:: default
 #
-# Copyright 2012-2013, Jonathan Hartman
+# Copyright 2012-2014, Jonathan Hartman
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
