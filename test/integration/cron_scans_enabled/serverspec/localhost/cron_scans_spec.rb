9b17e2e77f54bdd7867f8eed93d3f2b186e40b11
Jonathan Hartman <j@p4nt5.com>
2014-01-22 16:06:32 +0000

    Fix cron scan integration tests

--- test/integration/cron_scans_enabled/serverspec/localhost/cron_scans_spec.rb
+++ test/integration/cron_scans_enabled/serverspec/localhost/cron_scans_spec.rb
@@ -4,10 +4,12 @@ require 'spec_helper'
 
 describe 'clamav cron scans' do
   let(:script) { '/usr/local/bin/clamav-scan.sh' }
-  let(:minimal_cmd) do
+  let(:min_file) { '/etc/cron.d/clamav_minimal_scan' }
+  let(:min_cmd) do
     "#{script} /bin /sbin /usr/bin /usr/sbin /usr/local/bin " +
       '/usr/local/sbin /etc /root /opt /home'
   end
+  let(:full_file) { '/etc/cron.d/clamav_full_scan' }
   let(:full_cmd) { "#{script} /" }
 
   it 'has the scan script in place' do
@@ -15,11 +17,11 @@ describe 'clamav cron scans' do
   end
 
   it 'has the minimal scan cron job enabled' do
-    expect(cron).to have_entry("42 0 * * 1-6 #{minimal_cmd}")
+    expect(file(min_file).content).to include("42 0 * * 1-6 root #{min_cmd}")
   end
 
   it 'has the full scan cron job enabled' do
-    expect(cron).to have_entry("42 0 * * 0 #{full_cmd}")
+    expect(file(full_file).content).to include("42 0 * * 0 root #{full_cmd}")
   end
 end
 
