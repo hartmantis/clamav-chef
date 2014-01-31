4b6434d4e4fa4f798d2d9577c8044ee4429b2ee0
Jonathan Hartman <j@p4nt5.com>
2014-01-31 13:45:52 +0000

    Version bump to 1.0.0

--- metadata.rb
+++ metadata.rb
@@ -6,7 +6,7 @@ maintainer_email  'j@p4nt5.com'
 license           'Apache v2.0'
 description       'Installs/configures ClamAV'
 long_description  IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version           '0.4.2'
+version           '1.0.0'
 
 depends           'logrotate', '~> 1.0'
 depends           'yum', '~> 3.0'
