f0ac4dee8b3df7bba21544c30275b2c1d351d8ed
Jonathan Hartman <j@p4nt5.com>
2015-01-27 15:06:45 -0800

    Update deprecated matcher declarations

--- spec/support/matchers/apt_repository.rb
+++ spec/support/matchers/apt_repository.rb
@@ -2,19 +2,12 @@
 
 require 'spec_helper'
 
-module ChefSpec
-  module API
-    # Some simple matchers for the apt_repository resource
-    #
-    # @author Jonathan Hartman <j@p4nt5.com>
-    module AptRepositoryMatchers
-      ChefSpec::Runner.define_runner_method :apt_repository
+if defined?(ChefSpec)
+  ChefSpec.define_matcher(:apt_repository)
 
-      def create_apt_repository(resource_name)
-        ChefSpec::Matchers::ResourceMatcher.new(:apt_repository,
-                                                :create,
-                                                resource_name)
-      end
-    end
+  def create_apt_repository(resource_name)
+    ChefSpec::Matchers::ResourceMatcher.new(:apt_repository,
+                                            :create,
+                                            resource_name)
   end
 end
--- spec/support/matchers/cron_d.rb
+++ spec/support/matchers/cron_d.rb
@@ -2,17 +2,10 @@
 
 require 'spec_helper'
 
-module ChefSpec
-  module API
-    # Some simple matchers for the cron_d resource
-    #
-    # @author Jonathan Hartman <j@p4nt5.com>
-    module CronDMatchers
-      ChefSpec::Runner.define_runner_method :cron_d
+if defined?(ChefSpec)
+  ChefSpec.define_matcher(:cron_d)
 
-      def create_cron_d(resource_name)
-        ChefSpec::Matchers::ResourceMatcher.new(:cron_d, :create, resource_name)
-      end
-    end
+  def create_cron_d(resource_name)
+    ChefSpec::Matchers::ResourceMatcher.new(:cron_d, :create, resource_name)
   end
 end
