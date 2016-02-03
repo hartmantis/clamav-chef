ad37adc59700ed8566d9c2ee2403a8d5f7bd99e7
Jonathan Hartman <j@p4nt5.com>
2016-02-02 16:29:54 -0800

    Ensure /sbin and /usr/sbin are in test env PATH

--- test/integration/default/serverspec/spec_helper.rb
+++ test/integration/default/serverspec/spec_helper.rb
@@ -2,4 +2,11 @@
 
 require 'serverspec'
 
-set :backend, :exec
+ENV['PATH'] = (ENV['PATH'].split(':') + %w(/sbin /usr/sbin)).uniq.join(':')
+
+if RUBY_PLATFORM =~ /mswin|mingw32|windows/
+  set :os, family: 'windows'
+  set :backend, :cmd
+else
+  set :backend, :exec
+end
