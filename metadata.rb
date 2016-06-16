fa65d15bb3bfe62ca20385c8e2e5f4349900892f
vanliew <vanliew@adobe.com>
2016-06-16 15:03:51 +0200

    bumps apt dependency to ~> 3.0 requiring chef 11+

--- metadata.rb
+++ metadata.rb
@@ -11,7 +11,7 @@ version           '1.3.1'
 depends           'logrotate', '~> 1.0'
 depends           'yum', '~> 3.0'
 depends           'yum-epel', '~> 0.2'
-depends           'apt', '~> 2.1'
+depends           'apt', '~> 3.0'
 # Note that a breaking bug was introduced in 1.3.10 and fixed in 1.3.12, but
 # we really don't want a ">=" cookbook dep situation here
 depends           'cron', '~> 1.2'
