86846c8e6b7977d5a9c278bd421bf8d93b8044d1
Arun Bhagyanath <arun.bhagyanath1@hcentive.com>
2016-07-05 06:44:21 +0000

    Implemeneted private repo feature in freshclam

--- metadata.rb
+++ metadata.rb
@@ -6,7 +6,7 @@ maintainer_email  'j@p4nt5.com'
 license           'Apache v2.0'
 description       'Installs/configures ClamAV'
 long_description  'Installs/configures ClamAV'
-version           '1.3.1'
+version           '1.3.2'
 
 depends           'logrotate', '~> 1.0'
 depends           'yum', '~> 3.0'
