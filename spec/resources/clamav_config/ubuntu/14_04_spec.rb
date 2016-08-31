1b2ec1d7436fe67c338621723700be593eff5f60
Jonathan Hartman <j@p4nt5.com>
2016-08-31 12:18:09 -0700

    Split out the common-to-Debian pieces of the config tests

--- spec/resources/clamav_config/ubuntu/14_04_spec.rb
+++ spec/resources/clamav_config/ubuntu/14_04_spec.rb
@@ -1,16 +1,13 @@
 # encoding: utf-8
 # frozen_string_literal: true
 
-require_relative '../../clamav_config'
+require_relative '../debian'
 
 describe 'resources::clamav_config::ubuntu::14_04' do
-  include_context 'resources::clamav_config'
+  include_context 'resources::clamav_config::debian'
 
   let(:platform) { 'ubuntu' }
   let(:platform_version) { '14.04' }
-  let(:config_dir) { '/etc/clamav' }
-  let(:user) { 'clamav' }
-  let(:group) { 'clamav' }
 
-  it_behaves_like 'any platform'
+  it_behaves_like 'any Debian platform'
 end
