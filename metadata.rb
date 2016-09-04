9ea0e3f0067e271e1fa7df815bc8fa875033d726
Jonathan Hartman <j@p4nt5.com>
2016-09-04 15:34:32 -0700

    Remove all deps and supported platforms except Debians (for now)

--- metadata.rb
+++ metadata.rb
@@ -1,21 +1,16 @@
-# Encoding: UTF-8
+# encoding: utf-8
+# frozen_string_literal: true
 
-name              'clamav'
-maintainer        'Jonathan Hartman'
-maintainer_email  'j@p4nt5.com'
-license           'Apache v2.0'
-description       'Installs/configures ClamAV'
-long_description  'Installs/configures ClamAV'
-version           '1.3.1'
+name 'clamav'
+maintainer 'Jonathan Hartman'
+maintainer_email 'j@p4nt5.com'
+license 'Apache v2.0'
+description 'Installs/configures ClamAV'
+long_description 'Installs/configures ClamAV'
+version '1.3.1'
 
-depends           'logrotate', '~> 1.0'
-depends           'yum', '~> 3.0'
-depends           'yum-epel', '~> 0.2'
-depends           'cron', '~> 1.2'
+source_url 'https://github.com/roboticcheese/clamav-chef'
+issues_url 'https://github.com/roboticcheese/clamav-chef/issues'
 
-supports          'ubuntu'
-supports          'debian'
-supports          'redhat'
-supports          'centos'
-supports          'scientific'
-supports          'amazon'
+supports 'ubuntu'
+supports 'debian'
