a4c539015025490171ca3fed2610895750973b84
Jonathan Hartman <j@p4nt5.com>
2016-09-05 14:57:13 -0700

    Convert the clamav_app resource to a custom resource

---
+++ spec/resources/clamav_app/debian.rb
@@ -0,0 +1,15 @@
+# encoding: utf-8
+# frozen_string_literal: true
+
+require_relative '../clamav_app'
+
+shared_context 'resources::clamav_app::debian' do
+  include_context 'resources::clamav_app'
+
+  let(:base_packages) { %w(clamav clamav-daemon clamav-freshclam) }
+  let(:dev_packages) { %w(libclamav-dev) }
+
+  shared_examples_for 'any Debian platform' do
+    it_behaves_like 'any platform'
+  end
+end
--- spec/resources/clamav_app/ubuntu/14_04_spec.rb
+++ spec/resources/clamav_app/ubuntu/14_04_spec.rb
@@ -1,115 +1,13 @@
-require_relative '../../../spec_helper'
+# encoding: utf-8
+# frozen_string_literal: true
 
-describe 'resource_clamav_app::ubuntu::14_04' do
-  let(:version) { nil }
-  let(:dev) { nil }
-  let(:action) { nil }
-  let(:runner) do
-    ChefSpec::SoloRunner.new(
-      step_into: 'clamav_app', platform: 'ubuntu', version: '14.04'
-    ) do |node|
-      node.set['clamav']['version'] = version
-      node.set['clamav']['dev'] = dev
-    end
-  end
-  let(:converge) { runner.converge("resource_clamav_app_test::#{action}") }
+require_relative '../debian'
 
-  context 'the default action (:install)' do
-    let(:action) { :default }
+describe 'resources::clamav_app::ubuntu::14_04' do
+  include_context 'resources::clamav_app::debian'
 
-    shared_examples_for 'any attribute set' do
-      %w(clamav clamav-daemon clamav-freshclam).each do |p|
-        it "installs the #{p} package" do
-          expect(chef_run).to install_package(p).with(version: version)
-        end
-      end
-    end
+  let(:platform) { 'ubuntu' }
+  let(:platform_version) { '14.04' }
 
-    context 'all default attributes' do
-      cached(:chef_run) { converge }
-
-      it_behaves_like 'any attribute set'
-
-      it 'does not install the dev package' do
-        expect(chef_run).to_not install_package('libclamav-dev')
-      end
-    end
-
-    context 'an overridden version attribute' do
-      let(:version) { '1.2.3' }
-      cached(:chef_run) { converge }
-
-      it_behaves_like 'any attribute set'
-
-      it 'does not install the dev package' do
-        expect(chef_run).to_not install_package('libclamav-dev')
-      end
-    end
-
-    context 'an overridden dev attribute' do
-      let(:dev) { true }
-      cached(:chef_run) { converge }
-
-      it_behaves_like 'any attribute set'
-
-      it 'installs the dev package' do
-        expect(chef_run).to install_package('libclamav-dev')
-      end
-    end
-  end
-
-  context 'the :upgrade action' do
-    let(:action) { :upgrade }
-
-    shared_examples_for 'any attribute set' do
-      %w(clamav clamav-daemon clamav-freshclam).each do |p|
-        it "upgrades the #{p} package" do
-          expect(chef_run).to upgrade_package(p).with(version: version)
-        end
-      end
-    end
-
-    context 'all default attributes' do
-      cached(:chef_run) { converge }
-
-      it_behaves_like 'any attribute set'
-
-      it 'does not upgrade the dev package' do
-        expect(chef_run).to_not upgrade_package('libclamav-dev')
-      end
-    end
-
-    context 'an overridden version attribute' do
-      let(:version) { '1.2.3' }
-      cached(:chef_run) { converge }
-
-      it_behaves_like 'any attribute set'
-
-      it 'does not upgrade the dev package' do
-        expect(chef_run).to_not upgrade_package('libclamav-dev')
-      end
-    end
-
-    context 'an overridden dev attribute' do
-      let(:dev) { true }
-      cached(:chef_run) { converge }
-
-      it_behaves_like 'any attribute set'
-
-      it 'upgrades the dev package' do
-        expect(chef_run).to upgrade_package('libclamav-dev')
-      end
-    end
-  end
-
-  context 'the :remove action' do
-    let(:action) { :remove }
-    cached(:chef_run) { converge }
-
-    %w(clamav clamav-daemon clamav-freshclam libclamav-dev).each do |p|
-      it "removes the #{p} package" do
-        expect(chef_run).to remove_package(p)
-      end
-    end
-  end
+  it_behaves_like 'any Debian platform'
 end
