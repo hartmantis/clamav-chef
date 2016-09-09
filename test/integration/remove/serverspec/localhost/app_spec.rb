11cdb026c21105e32a7142af0a12ada2b088dc6c
Jonathan Hartman <j@p4nt5.com>
2016-09-09 15:59:24 -0700

    Fix failing ServerSpec tests

--- test/integration/remove/serverspec/localhost/app_spec.rb
+++ test/integration/remove/serverspec/localhost/app_spec.rb
@@ -1,6 +1,7 @@
-# Encoding: UTF-8
+# encoding: utf-8
+# frozen_string_literal: true
 
-require '../spec_helper'
+require_relative '../spec_helper'
 
 describe 'clamav::remove::app' do
   describe package('clamav') do
--- test/integration/remove/serverspec/localhost/service_spec.rb
+++ test/integration/remove/serverspec/localhost/service_spec.rb
@@ -1,6 +1,7 @@
-# Encoding: UTF-8
+# encoding: utf-8
+# frozen_string_literal: true
 
-require '../spec_helper'
+require_relative '../spec_helper'
 
 describe 'clamav::remove::service' do
   %w(clamav-daemon clamav-freshclam).each do |s|
