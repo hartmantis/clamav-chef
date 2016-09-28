eeccdc1941465c1d8d0e60d5934c5eccf3ed145c
Jonathan Hartman <j@p4nt5.com>
2016-09-28 16:05:07 -0700

    Depend on Chef 12+

--- metadata.rb
+++ metadata.rb
@@ -8,6 +8,7 @@ license 'Apache v2.0'
 description 'Installs/configures ClamAV'
 long_description 'Installs/configures ClamAV'
 version '1.3.1'
+chef_version '>= 12'
 
 source_url 'https://github.com/roboticcheese/clamav-chef'
 issues_url 'https://github.com/roboticcheese/clamav-chef/issues'
