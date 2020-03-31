55e5497fb4b8e8f6e87dcb43276a40bf11df9366
Edgaras Lukoševičius <edgaras.lukosevicius@gmail.com>
2020-03-31 15:59:54 +0300

    Update package names for CentOS >= 7

--- metadata.rb
+++ metadata.rb
@@ -5,7 +5,7 @@ maintainer_email 'j@p4nt5.com'
 license 'Apache-2.0'
 description 'Installs/configures ClamAV'
 long_description 'Installs/configures ClamAV'
-version '1.3.3'
+version '1.3.4'
 chef_version '>= 12'
 
 source_url 'https://github.com/roboticcheese/clamav-chef'
